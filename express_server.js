16cde225c010bf4136638f05729ccd48a3deea36
Paul Chen <paulchen647@gmail.com>
2020-09-29 22:02:56 +0000

    Implemented cookies

--- express_server.js
+++ express_server.js
@@ -1,21 +1,28 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
+
+
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
 app.set("view engine", "ejs");
 
 function generateRandomString() { //random string for shortened url
-  let result = ''; 
+  let result = '';
   const charList = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  for( let i = 0; i < 6; i++){
+  for (let i = 0; i < 6; i++) {
     const newIndex = Math.floor(Math.random() * charList.length);
     result += charList[newIndex];
   }
   return result;
 }
 
-const urlDatabase = { 
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -34,16 +41,28 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => { //indexes all urls and its shortened url
-  const templateVars = { urls: urlDatabase};
+  console.log(req.cookies);
+  const templateVars = {
+    username: req.cookies['username'],
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => { // POST new url
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+    // ... any other vars
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => { //GET url
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+
+  const templateVars = {
+    username: req.cookies['username'],
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
@@ -51,7 +70,7 @@ app.get("/u/:shortURL", (req, res) => {
   //console.log(req.body);
   const longURL = urlDatabase[req.params.shortURL];
   console.log(longURL);
-  if(longURL){
+  if (longURL) {
     res.redirect(longURL);
   } else {
     
@@ -74,14 +93,31 @@ app.post("/urls/:shortURL", (req, res) => {
   urlDatabase[shortURL] = longURL;
   
   res.redirect(`/urls/${shortURL}`);
-})
+});
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
 
   res.redirect('/urls');
-})
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie('username', username);
+
+
+  res.redirect('/urls');
+});
+
+app.post("/logout", (req, res) => {
+  const username = req.body.username;
+  res.clearCookie('username');
+
+
+  res.redirect('/urls');
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
